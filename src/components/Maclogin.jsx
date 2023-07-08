97499179a7d3e2cfc922dbc0109ba9a28cde8912
Rajesh Ramasamy <rrajesh@genixtec.com>
2023-07-08 21:42:00 +0530

    response set

--- src/components/Maclogin.jsx
+++ src/components/Maclogin.jsx
@@ -1,5 +1,5 @@
 import AppleSignin from 'react-apple-signin-auth';
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 import AppleIcon from '@mui/icons-material/Apple';
@@ -9,76 +9,78 @@ import './socialiconstyle.css'
 
 
 
-export default function Maclogin()  {
+export default function Maclogin() {
 
-    const [appleData, setappleData] = useState({})
+  const [appleData, setappleData] = useState({})
 
 
-    const responseapple = (response) => {
-    
+  const responseapple = (response) => {
+
     console.log(response);
-};
-
-    
+  };
 
 
-    return (
-        <div>
- 
 
-      
- <AppleSignin
-    /** Auth options passed to AppleID.auth.init() */
-    authOptions={{
-      /** Client ID - eg: 'com.example.com' */
-      clientId: 'com.alphamale.dev',
-      /** Requested scopes, seperated by spaces - eg: 'email name' */
-      scope: 'email name',
-      /** Apple's redirectURI - must be one of the URIs you added to the serviceID - the undocumented trick in apple docs is that you should call auth from a page that is listed as a redirectURI, localhost fails */
-      redirectURI: 'https://albhamale.netlify.app/',
-      /** State string that is returned with the apple response */
-      state: 'orgin:web',
-      /** Nonce */
-      nonce: 'nonce',
-      /** Uses popup auth instead of redirection */
-       usePopup: true,
-       responseMode:"query",
-       callback:{responseapple}
-    }} // REQUIRED
-    /** General props */
-    uiType="dark"
-    /** className */
-    className="apple-auth-btn"
-    /** Removes default style tag */
-    noDefaultStyle={false}
-    /** Allows to change the button's children, eg: for changing the button text */
-    buttonExtraChildren="Continue with Apple"
-    /** Extra controlling props */
-    /** Called upon signin success in case authOptions.usePopup = true -- which means auth is handled client side */
-    onSuccess={(response) => console.log("success"+JSON.stringify(response))} // default = undefined
-    /** Called upon signin error */
-    onError={(error) => console.error("error" +JSON.stringify(error))} // default = undefined
-    /** Skips loading the apple script if true */
-    skipScript={false} // default = undefined
-    /** Apple image props */
-    iconProp={{ style: { marginTop: '10px' } }} // default = undefined
-   
-    //  render={(props) => <button {...props}>My Custom Button</button>}
 
-    render={(renderProps) => (
-      <div className="login-social-item login-social-item--apple">
-     <IconButton onClick={renderProps.onClick} className="login-social-item__image" sx={{ color: "white" }}>
-     <AppleIcon/>
-    </IconButton>
-     </div>
-   )}
-  />
-     
-    
- 
+  return (
+    <div>
 
-  
-  </div>
+
+
+      <AppleSignin
+
+        authOptions={{
+
+          clientId: 'com.alphamale.dev',
+
+          scope: 'email name',
+
+          redirectURI: 'https://albhamale.netlify.app/',
+
+          state: 'orgin:web',
+
+          nonce: 'nonce',
+
+          usePopup: true,
+          responseMode: "query",
+          // callback: { responseapple }
+        }}
+        uiType="dark"
+
+        className="apple-auth-btn"
+
+        noDefaultStyle={false}
+
+        buttonExtraChildren="Continue with Apple"
+
+        // onSuccess={(response) => console.log("success"+JSON.stringify(response))} 
+
+        // onError={(error) => console.error("error" +JSON.stringify(error))} 
+
+        onSuccess={responseapple}
+
+        onError={responseapple}
+
+        skipScript={false}
+
+        iconProp={{ style: { marginTop: '10px' } }}
+
+
+
+        render={(renderProps) => (
+          <div className="login-social-item login-social-item--apple">
+            <IconButton onClick={renderProps.onClick} className="login-social-item__image" sx={{ color: "white" }}>
+              <AppleIcon />
+            </IconButton>
+          </div>
+        )}
+      />
+
+
+
+
+
+    </div>
   );
 }
 
