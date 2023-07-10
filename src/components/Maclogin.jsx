f77fcf538eea9b9688adbeb9fdbd8d27a6bc35f7
Rajesh Ramasamy <rrajesh@genixtec.com>
2023-07-10 09:05:54 +0530

    api url connect

--- src/components/Maclogin.jsx
+++ src/components/Maclogin.jsx
@@ -19,7 +19,7 @@ export default function Maclogin() {
   const responseapple = async(response) => {
 
 
-    console.log(response.user);
+    console.log(response.user.email);
     // console.log("email" +response.user.email);
     // console.log("name" +response.user.name);
 
